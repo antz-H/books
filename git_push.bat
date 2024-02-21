c758c993b9138f4348cc749ab7e1aa347b4eec60
Ethan.huang <417213902@qq.com>
2024-02-21 11:52:25 +0800

    "

--- git_push.bat
+++ git_push.bat
@@ -6,10 +6,10 @@ git add *
 echo\
 echo 2. Commit the changes to the local repository
 
-set /p commit_msg="Enter your custom commit message: "
+set /p commit_msg="Enter your custom commit message (press Enter for default): "
 if "%commit_msg%"=="" (
-    set now=%date:~0,10% %time%
-    echo "Time:" %now%
+    set "now=%date:~0,10% %time%"
+    echo "commit_msg:" %now%
     git commit -m "%now%"
 ) else (
     git commit -m "%commit_msg%"
